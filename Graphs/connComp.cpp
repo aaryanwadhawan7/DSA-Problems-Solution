9dc9605b47e042ab0d8fdcc2dd222d0f441e9962
aaryanwadhawan7 <aaryanwadhawam339@gmail.com>
2025-08-15 21:14:10 +0530

    Connected components

--- Graphs/connComp.cpp
+++ Graphs/connComp.cpp
@@ -1,3 +1,90 @@
 // Connected Components [Undirected Graph] 
 // It is a graph only but broken down into 4 different components !
 // Code will be covered later !
+
+#include <iostream>
+#include <vector> 
+#include <queue>
+using namespace std;
+
+void printAdjLst (int n, int m, vector <int> adj[]) {
+    for (int i = 0; i < n; i++) {
+        cout << i << " -> ";
+        for (auto itr : adj[i]) {
+            cout << itr << " ";
+        }
+        cout << endl;
+    }
+}
+
+void bfs (vector <int> &comp, int visited[], vector <int> adj[], int start) {
+    queue <int> q;
+    q.push (start);
+
+    // traverse the neighbour of the node 
+    while (!q.empty()) {
+        // node -> starting node
+        int node = q.front();
+        q.pop();
+        visited [node] = 1;
+        comp.push_back (node);
+
+        for (auto itr : adj[node]) {
+            if (!visited[itr]) {
+                q.push (itr);
+                visited[itr] = 1;
+            }
+        }
+    }
+}
+
+int main () {
+    // Undirected Graph
+    // Breadth First Search (BFS)
+    // 0-based indexing
+
+    int n, m;
+    cin >> n >> m;
+
+    vector <int> adj[n+1];
+    for (int i = 0; i < m; i++) {
+        // i -> 0 to m
+        int u, v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+    cout << endl;
+
+    cout << "Adjacency List : " << endl;
+    printAdjLst (n, m, adj);
+    cout << endl;
+
+    vector <vector<int>> lst; // List of Connected Components
+    int visited[n+1] = {0};
+    for (int i = 0; i < n; i++) {
+        vector <int> comp;
+        if (!visited[i]) {
+            bfs (comp, visited, adj, i);
+            lst.push_back(comp);
+        }
+    }
+
+    // cout << "Breadth First Search : " << endl;
+    // for (auto itr : lst) {
+    //     cout << itr << " ";
+    // }
+    // cout << endl;
+
+    for (auto comp : lst) {
+        for (auto &node : comp) {
+            cout << node << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+
+    cout << "No. of Connected Components : " << lst.size();
+
+    return 0;
+}
--- Graphs/dfs.cpp
+++ Graphs/dfs.cpp
@@ -2,43 +2,36 @@
 #include <vector>
 using namespace std;
 
-void printAdjLst(int n, int m, vector<int> adj[])
-{
+void printAdjLst (int n, int m, vector <int> adj[]) {
     // n -> # of nodes/vertices
     // m -> # of edges
 
-    for (int i = 0; i <= n; i++)
-    {
+    for (int i = 0; i <= n; i++) {
         cout << i << " -> ";
-        for (auto itr : adj[i])
-        {
+        for (auto itr : adj[i]) {
             cout << itr << " ";
         }
         cout << endl;
     }
 }
 
-void dfs(int node, vector<int> adj[], vector<int> &lst, int visited[])
-{
+void dfs (int node, vector<int> adj[], vector <int> &lst, int visited[]) {
     lst.push_back(node);
     // mark occurence of node in visited arr
     visited[node] = 1;
 
-    for (auto itr : adj[node])
-    {
+    for (auto itr : adj[node]) {
         if (!visited[itr])
-            dfs(itr, adj, lst, visited);
+        dfs (itr, adj, lst, visited);
     }
 }
 
-int main()
-{
+int main () {
     int n, m;
     cin >> n >> m;
-    vector<int> adj[n + 1]; // adjacency list
+    vector <int> adj[n+1]; // adjacency list
 
-    for (int i = 0; i < m; i++)
-    {
+    for (int i = 0; i < m; i++) {
         int u, v;
         cin >> u >> v;
         adj[u].push_back(v);
@@ -46,16 +39,15 @@ int main()
     }
     cout << endl;
 
-    printAdjLst(n, m, adj);
+    printAdjLst (n, m, adj);
 
     cout << "Depth First Traversal : " << endl;
     int startNode = 1;
-    vector<int> lst;
-    int visited[n + 1] = {0};
-    dfs(startNode, adj, lst, visited);
+    vector <int> lst;
+    int visited[n+1] = {0};
+    dfs (startNode, adj, lst, visited);
 
-    for (auto itr : lst)
-    {
+    for (auto itr : lst) {
         cout << itr << " ";
     }
 
